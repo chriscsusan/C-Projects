c84b154759661cd568496bf2f9472686a2a48965
Chris Susan <chriscsusan@gmail.com>
2016-10-24 11:35:36 -0400

    Initial commit

---
+++ FirstProgram/FirstProgram/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Circle
+    {
+        private double radius;
+        private static int numOfCircles;
+
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            numOfCircles++;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getCircumference()
+        {
+            return Math.PI * (2 * radius);
+        }
+
+        public double getArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public static int getObjectCount()
+        {
+            return numOfCircles;
+        }
+
+        public static string formatNumber(double input)
+        {
+            return input.ToString("#.##");
+        }
+    }
+}
---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static string reverse(string input)
+        {
+            string output = "";
+            char[] temp = input.ToCharArray();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                output += temp[i];
+            }
+
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            //for(i = 0; i<=100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Console.WriteLine("Hello World");
+            Console.WriteLine(reverse("Usher us into the dreaming."));
+
+            Console.ReadLine();
+
+        }
+    }
+}
